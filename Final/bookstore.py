e801a6fe1eb6a2a1a62e2827d7b104be9cde4c87
apar2021 <45519735+apar2021@users.noreply.github.com>
2023-07-19 18:52:38 -0400

    Update bookstore.py

--- Final/bookstore.py
+++ Final/bookstore.py
@@ -1,6 +1,42 @@
 from flask import Flask,render_template
+from flask import UserMixin
 app = Flask(__name__)
 
+
+class User(db.Model,UserMixin):
+     __tablename__="users"
+    id = db.Column(db.Integer(),primary_key=True)
+    username = db.Column(db.String(length=30),nullable=False,unique=True)
+    first_name = db.Column(db.String(length=30),nullable=False)
+    last_name = db.Column(db.String(length=30),nullable=False)
+    password_hash = db.Column(db.Integer(),nullable=False,default=1000)
+    created_on = db.Column(db.DateTime, nullable=False)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+    is_confirmed = db.Column(db.Boolean, nullable=False, default=False)
+    confirmed_on = db.Column(db.DateTime, nullable=True)
+    @property
+    def password(self):
+        return self.password
+    @password.setter
+    def password(self,plain_text_password):
+        self.password_hash = Bcrypt.generate_password_hash(plain_text_password).decode('UTF-8')
+class Item(db.Model):
+    id = db.Column(db.Integer(),primary_key=True)
+    isbn = db.Column(db.String(length=15),nullable=False,unique=True)
+    title = db.Column(db.String(length=1024),nullable=False)
+    author = db.Column(db.String(length=150),nullable=False)
+    
+    edition = db.Column(db.String(length=150),nullable=False)
+    catergory = db.Column(db.String(length=150),nullable=False)
+    publisher = db.Column(db.String(length=1024),nullable=False)
+    publication_year = db.Column(db.String(length=30),nullable=False)
+    
+    quantity = db.Column(db.Integer(),nullable=False)
+    min_threshold = db.Column(db.Integer(),nullable=False)
+    buying_price = db.Column(db.Float(),nullable=False)
+    selling_price = db.Column(db.Float(),nullable=False)
+    
+    
 @app.route('/')
 @app.route('/home')
 def home_page():
