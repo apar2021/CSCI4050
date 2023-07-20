8323034cb99f9d83eaabde94afed822188af1953
apar2021 <45519735+apar2021@users.noreply.github.com>
2023-07-20 00:14:06 -0400

    Update forms.py

--- backend/code/website/forms.py
+++ backend/code/website/forms.py
@@ -4,6 +4,7 @@ from wtforms.validators import DataRequired, Email, Length, NumberRange, EqualTo
 
 class RegistrationForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(max=150)])
+    username = StringField('Username', validators=[DataRequired()])
     phone = IntegerField('Phone', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email(), Length(max=150)])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -24,5 +25,9 @@ class ResetPasswordForm(FlaskForm):
     confirm_password = PasswordField('Confirm New Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Reset Password')
 
-
+class EditAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = HiddenField('Email', validators=[Email()])
+    
+    submit = SubmitField('Sign In')
     
