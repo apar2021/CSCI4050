1db27b3c5a246c7f27ce52a2c60c953a30a5d34f
apar2021 <45519735+apar2021@users.noreply.github.com>
2023-07-19 22:52:34 -0400

    Update bookstore.py

--- Final/bookstore.py
+++ Final/bookstore.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template
 from flask import UserMixin
+import smtplib
 app = Flask(__name__)
 
 class User(db.Model,UserMixin):
@@ -8,11 +9,14 @@ class User(db.Model,UserMixin):
     username = db.Column(db.String(length=30),nullable=False,unique=True)
     first_name = db.Column(db.String(length=30),nullable=False)
     last_name = db.Column(db.String(length=30),nullable=False)
+    phone_number = db.Column(db.String(length=15),nullable=False)
+    email_address=db.Column(db.String(min_length=9),nullable=False)
     password_hash = db.Column(db.Integer(),nullable=False,default=1000)
     created_on = db.Column(db.DateTime, nullable=False)
     is_admin = db.Column(db.Boolean, nullable=False, default=False)
     is_confirmed = db.Column(db.Boolean, nullable=False, default=False)
     confirmed_on = db.Column(db.DateTime, nullable=True)
+
     @property
     def password(self):
         return self.password
@@ -26,8 +30,13 @@ def home_page():
 @app.route('/login')
 def login_page():
     return render_template('login.html')
-@app.route('/register')
+@app.route('/register',methods=["POST"])
 def register():
+     first_name=request.form.get("first_name")
+     last_name = request.form.get("last_name")
+     phone_number = request.form.get("phone_number")
+     email_address = request.form.get("email_address")
+     
     return render_template('register.html')
 if __name__=="__main__":
     app.run(debug=True)
