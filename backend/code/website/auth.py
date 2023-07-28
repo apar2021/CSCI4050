338e50f60f7ce805cb551f500fae98b4e4739072
Jackson Cown <83671586+JacksonCown@users.noreply.github.com>
2023-07-28 15:28:00 -0400

    Add Comments

--- backend/code/website/auth.py
+++ backend/code/website/auth.py
@@ -12,18 +12,19 @@ from cryptography.fernet import Fernet
 
 auth = Blueprint('auth', __name__)
 
-
+# Method to verify user login information
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    # Check if form is valid
     if form.validate_on_submit():
         # Check if the user exists in the database
         user = User.query.filter_by(email=form.email.data).first()
-
+        # Check if the password is correct
         if user and check_password_hash(user.password, form.password.data):
+            # If if the user's email is verified
             if user.is_verified:
                 login_user(user)
-
                 flash('Login successful!', 'success')
                 return redirect(url_for('views.home'))
             else:
@@ -31,23 +32,24 @@ def login():
         else:
             flash('Invalid email or password. Please try again.', 'error')
     else:
+        # Outputting Errors
         for error, message in zip(form.errors.keys(), form.errors.values()):
             flash(f'{error.capitalize()} Error: {message[0]}')
 
     return render_template('Login.html', form = form)
 
-
+# Method to verify a user's registration information and create a new user
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = RegistrationForm()
-
+    # Check if form is valid
     if form.validate_on_submit():
         # Check if the email is already registered
         existing_user = User.query.filter_by(email=form.email.data).first()
         if existing_user:
             flash('Email already registered. Please log in instead.', 'error')
             return redirect(url_for('auth.login'))  # Redirect to login page if user already exists
-        
+        # Check if the password and confirm password fields match
         if form.password.data != form.confirm_password.data:
             flash('Passwords do not match. Please try again.', 'error')
             return redirect(url_for('auth.signup'))
@@ -60,6 +62,7 @@ def signup():
             password= generate_password_hash(form.password.data, 'sha256')
         )
 
+        # Record email for verification
         email = form.email.data
         new_user.generate_verification_token()
 
@@ -69,6 +72,7 @@ def signup():
         db.session.add(new_user)
         db.session.commit()
 
+        # Send the verification email
         msg = Message('Account Verification', sender='your_gmail_username', recipients=[email])
         msg.body = f'Click the following link to verify your account: {url_for("auth.verify_account", token=token, _external=True)}'
         mail.send(msg)
@@ -76,57 +80,59 @@ def signup():
         flash('Registration successful! You may proceed to the email verification page.', 'success')
         return redirect(url_for('auth.email_verification'))
     else:
+        # Outputting Errors
         for error, message in zip(form.errors.keys(), form.errors.values()):
             flash(f'{error.capitalize()} Error: {message[0]}')
 
     return render_template('Registration.html', form = form)
 
-
+# Method to update a user's verification status
 @auth.route('/verify-account/<token>')
 def verify_account(token):
+    # Query for user data
     user = User.query.filter_by(verification_token=token).first()
 
+    # Check if the user exists and if the token is valid
     if not user or user.verification_token_expiration < datetime.datetime.utcnow():
         flash('Invalid or expired verification token.', 'error')
         return redirect(url_for('auth.login'))
 
+    # Update the database to verify the user's account
     user.is_verified = True
     user.verification_token = None
     user.verification_token_expiration = None
-
     db.session.commit()
 
     flash('Your account has been verified successfully. You can now log in.', 'success')
     return redirect(url_for('auth.login'))  # Redirect to the login page
 
-
+# Method to redirect user view to confirm email confirmation
 @auth.route('/email-verification')
 def email_verification():
     return render_template('RegistrationEmailVerification.html')
 
-
+# Method to log out a user
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
-
     # Redirect the user to the desired page (e.g., home page or login page)
     return redirect(url_for('views.home'))
 
-
+# Method to send a forgot password email
 @auth.route('/forgot-password', methods=['GET', 'POST'])
 def forgot_password_email():
     form = ResetPasswordEmailForm()
-
+    # Check if the form is valid
     if form.validate_on_submit():
-        # Process the form data here
+        # Process the form data
         email = form.email.data
-
+        # Find the user's account in the database via email
         user = User.query.filter_by(email=email).first()
         
+        # If the user exists, generate a reset token and send the reset password email
         if user:
             token = secrets.token_urlsafe(16)  # Generate a token with 16 bytes (128 bits)
-
             user.reset_token = token
             user.reset_token_expiration = datetime.datetime.utcnow() + datetime.timedelta(hours=1)  # Set expiration to one hour from now
             db.session.commit()
@@ -137,28 +143,25 @@ def forgot_password_email():
             msg.body = f'Click the following link to reset your password: {url_for("auth.reset_password", token=token, _external=True)}'
             # Send the email
             mail.send(msg)
-            
             return render_template('ResetPasswordCorrectEmail.html')
         else: 
             return render_template('ResetPasswordIncorrectEmail.html')
 
-    
     return render_template('ResetPasswordEmail.html', form=form)
 
-
+# Method to reset a user's password
 @auth.route('/reset-password/<token>', methods=['GET', 'POST'])
 def reset_password(token):
-    # Here, you should implement logic to verify the token
+    # Check if the reset token is associated with a user account
     user = User.query.filter_by(reset_token=token).first()
-
     if not user:
         flash('Invalid token. Please request a new password reset.', 'error')
         return redirect(url_for('auth.forgot_password_email'))
 
     form = ResetPasswordForm()
-
+    # Validate Password Reset Data
     if form.validate_on_submit():
-       
+       # Check if the password and confirm password fields match
         if form.password.data != form.confirm_password.data:
             flash('Passwords do not match. Please try again.', 'error')
             return redirect(url_for('auth.reset_password', token=token))
@@ -173,14 +176,17 @@ def reset_password(token):
         # Save the changes to the database
         db.session.commit()
 
-        print(form.errors)
-
+        # Confirm password reset to user
         flash('Your password has been reset successfully. You can now log in with your new password.', 'success')
         return redirect(url_for('auth.login'))
-    print(form.errors)
+    else:
+        # Outputting Errors
+        for error, message in zip(form.errors.keys(), form.errors.values()):
+            flash(f'{error.capitalize()} Error: {message[0]}')
+
     return render_template('ResetPassword.html', form=form, token = token)
 
-
+# Method to edit a user's profile
 @auth.route('/edit-profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
@@ -200,7 +206,7 @@ def edit_profile():
         form.card_number.data = current_user.decrypt_card_number()
         form.expiration_date.data = current_user.expiration_date
         form.security_code.data = current_user.decrypt_security_code()
-
+    # Validate the form data
     if form.validate_on_submit():
         # Save the changes to the user's profile
         user = User.query.get(current_user.id)
@@ -212,11 +218,12 @@ def edit_profile():
         user.state = form.state.data
         user.zipcode = form.zipcode.data
 
+        # Encrypt the card number and security code
         user.card_type = form.card_type.data
         if form.card_number.data:
             user.card_number_encrypted = user.encrypt(str(form.card_number.data))
-
         user.expiration_date = form.expiration_date.data
+
         if form.security_code.data:
             user.security_code_encrypted = user.encrypt(str(form.security_code.data))
 
@@ -225,15 +232,19 @@ def edit_profile():
         
         db.session.commit()
 
+        # Send a confirmation email to the user
         msg = Message('Profile Updated', sender='your_gmail_username', recipients=[current_user.email])
         msg.body = 'Your profile has been updated successfully.'
         mail.send(msg)
 
-
+        # Confirm profile update to user
         flash('Your profile has been updated successfully.', 'success')
         return redirect(url_for('auth.edit_profile'))
+    else:
+        # Outputting Errors
+        for error, message in zip(form.errors.keys(), form.errors.values()):
+            flash(f'{error.capitalize()} Error: {message[0]}')
 
-    print(form.errors)
     return render_template('EditProfile.html', form=form)
 
 @auth.route('/add_to_cart', methods=['POST'])
