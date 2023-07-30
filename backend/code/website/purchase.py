0b6efe488d3fc71953f789470c526487aa3334e2
Jackson Cown <83671586+JacksonCown@users.noreply.github.com>
2023-07-29 20:03:07 -0400

    Cart Functional

--- backend/code/website/purchase.py
+++ backend/code/website/purchase.py
@@ -15,31 +15,14 @@ def add_to_cart(book_id, quantity=1):
             return redirect(url_for('views.home'))
     
     # Get the session cart or create a new one if it doesn't exist
-    print(1)
     cart = session.get('cart', {})
-    print(cart)
 
     # Add the book to the cart or update the quantity if it's already in the cart
-    print(2)
-    print(cart.get(book_id, 0))
     cart[str(book_id)] = cart.get(str(book_id), 0) + quantity
 
     session['cart'] = cart
-    print(3)
     print(cart)
 
-    # Check if the user already has a cart, if not, create one
-    #if not current_user.cart:
-    #    cart = Cart(userid=current_user.id)
-    #    db.session.add(cart)
-    #    db.session.commit()
-
-    # Find the user's cart and add the book to it
-    #cart = current_user.cart
-    #cart_item = CartItem(cartid=cart.id, bookid=book.id, quantity=quantity)
-    #db.session.add(cart_item)
-    #db.session.commit()
-    #session["cart"] = {}
     flash('The book has been added to your cart.', 'success')
     return redirect(url_for('views.home'))
 
